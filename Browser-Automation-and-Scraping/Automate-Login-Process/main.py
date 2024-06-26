1a1b03b7a4272687072125e6ef097bb29e18abed
Exabiier <102698808+Exabiier@users.noreply.github.com>
2024-06-25 19:11:58 -0700

    Leaned how to write data in text file

--- Browser-Automation-and-Scraping/Automate-Login-Process/main.py
+++ Browser-Automation-and-Scraping/Automate-Login-Process/main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 import time
+from datetime import datetime as dt
 
 # def get_driver():
 #     # Setup for my Webscraper
@@ -53,7 +54,14 @@ def clean_text(text):
     # it will only extract tempature:
     output = text.split(":")
     return float(output[1])
-    
+
+# This funtion will make .text file for each data we retrive from the website. 
+def write_function(text):
+    filename = f"{dt.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+    with open(filename, 'w') as file:
+        file.write(text)
+
+
 def main():
     driver = get_driver()
     driver.get("http://automated.pythonanywhere.com/login/")
@@ -62,7 +70,7 @@ def main():
     driver.find_element(by="id", value="id_username").send_keys("automated")
     driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
     driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-    
+
     # scrape portion for number:
     time.sleep(3)
     try:
