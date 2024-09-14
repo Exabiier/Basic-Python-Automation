4613cb1e65d6e8ec2c43c54cac63ae996f3c4ae1
Exabiier <102698808+Exabiier@users.noreply.github.com>
2024-09-13 18:48:21 -0700

    made progress on stock data

---
+++ Python_API/main.py
@@ -0,0 +1,27 @@
+import requests
+
+picker = input('Enter the stock symbol you want:')
+from_date = input('Enter Start data in mm/dd/yyyy format:')
+to_date = input('Enter end data in mm/dd/yyyy format:')
+
+
+url = f"https://www.marketwatch.com/investing/stock/csv/downloaddatapartial?startdate={from_date}%2000:00:00&enddate={to_date}%2000:00:00&daterange=d30&frequency=p1d&csvdownload=true&downloadpartial=false&newdates=false"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.marketwatch.com/"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    content = respnonse.content
+    print(content)
+    with open('data.csv', 'wb') as file:
+        file.write(content)
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+
+
